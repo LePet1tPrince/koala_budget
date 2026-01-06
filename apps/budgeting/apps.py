52965716914f58738e43084a4b696f9c2a625643
LePetitPrince <timmylbender@gmail.com>
2026-01-06 10:41:54 -0500

    deleted old budgeting app

--- apps/budgeting/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BudgetingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.budgeting"
-
-    def ready(self):
-        import apps.budgeting.signals  # noqa: F401
